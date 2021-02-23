be66fbbfde1a12438eb1128819b2f8e569c7eded
_Szwendacz_ <maciejlebiest2@gmail.com>
2021-02-23 19:10:34 +0100

    refactor to common crud interface, close #5

--- pet-clinic-data/src/main/java/com/maciej/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/maciej/petclinic/services/PetService.java
@@ -3,13 +3,9 @@ package com.maciej.petclinic.services;
 import com.maciej.petclinic.model.Pet;
 
 import java.time.LocalDate;
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
+public interface PetService extends CrudService<Pet,Long>{
 
     Pet findByBirthDate(LocalDate date);
 
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/maciej/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/maciej/petclinic/services/VetService.java
@@ -1,18 +1,9 @@
 package com.maciej.petclinic.services;
 
-import com.maciej.petclinic.model.Owner;
 import com.maciej.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long>{
 
     Vet findByLastName(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 
 }
